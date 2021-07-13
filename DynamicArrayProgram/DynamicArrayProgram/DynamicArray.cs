affa66161686e4affd1af4050d2af024d3c12a8a
srinivas-kv <srinivaskv88841@gmail.com>
2021-07-13 10:07:06 +0530

    commit

---
+++ DynamicArrayProgram/DynamicArrayProgram/DynamicArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArrayProgram
+{
+    class DynamicArray
+    {
+        int[] bufferArray;
+
+
+        public DynamicArray()
+        {
+            bufferArray = new int[6];
+        }
+        public DynamicArray(int initialSize)
+        {
+            bufferArray = new int[initialSize];
+        }
+
+
+        public int this[int index]
+        {
+            set
+            {
+                Array.Resize(ref bufferArray, index + 6);
+                bufferArray[index] = value;
+            }
+            get
+            {
+                return bufferArray[index];
+            }
+        }
+    }
+}
+
---
+++ DynamicArrayProgram/DynamicArrayProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArrayProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray _dynamicArray = new DynamicArray(6);
+
+            _dynamicArray[0] = 10;
+            _dynamicArray[1] = 20;
+            _dynamicArray[2] = 30;
+            _dynamicArray[3] = 40;
+            _dynamicArray[4] = 50;
+            _dynamicArray[5] = 60;
+            _dynamicArray[6] = 70;
+            int count = _dynamicArray[6];
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
